b04220f4813fb6fc603455bec48a4b44c8b07075
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 14:08:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,9 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       console.log(array.unshift("I am" + i + "strange loop."))
-  } else {
-    console.log(array.unshift(`I am ${i} strange loops`))
   }
 }
-
+}
 //    if (i === 1) {
 //      return array.push("I am 1 strange loop.")
 //    } else {
