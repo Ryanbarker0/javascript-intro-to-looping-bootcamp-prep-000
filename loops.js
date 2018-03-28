0666e70bdaca74570c8203008c5acbcba1587725
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-28 16:16:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   do {
     console.log(array)
-  } while (array.length > 0 && maybeTrue()) {
+  } while (array.length > 0 && maybeTrue(array)) {
       return array
     }
 }
